2bf22dca496187f0f98a68a504382a72efb7948e
akshat-kumar2109 <akshatkumar1101@gmail.com>
2022-07-04 09:39:19 +0530

    notification

--- app.js
+++ app.js
@@ -70,6 +70,34 @@ app.post("/upload", upload.single("file"), async (req, res) => {
     });
 });
 
+// PUSH NOTIFICATION
+const notification_options = {
+  priority: "high",
+  timeToLive: 60 * 60 * 24,
+};
+
+app.post("/notification", (req, res) => {
+  const registrationToken = req.body.registrationToken;
+  const message = req.body.message;
+  const options = notification_options;
+
+  admin
+    .messaging()
+    .sendToDevice(registrationToken, message, options)
+    .then((response) => {
+      res.status(200).send({
+        status: "sucess",
+        msg: "Notification send sucessfully",
+      });
+    })
+    .catch((error) => {
+      res.status(400).send({
+        status: "fail",
+        message: error,
+      });
+    });
+});
+
 app.set("emmiter", timerEventEmitter);
 app.use("/auth", authRoute);
 app.use("/shop", shopRoute);
