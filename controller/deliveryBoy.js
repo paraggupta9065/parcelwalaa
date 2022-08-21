0e94a6a8c1d3af966cfaf9cfadbc318d8c496c5f
paraggupta9065 <paraggupta9063@gmail.com>
2022-08-21 13:15:36 +0530

    add driver

--- controller/deliveryBoy.js
+++ controller/deliveryBoy.js
@@ -3,59 +3,68 @@ const otpModel = require("../model/otp");
 
 // create a delivery boy
 exports.addDeliveryBoy = async (req, res) => {
-  const image = "asad";
-  const driving_license_image = "adf";
-  const { number, name, otpCode } = req.body;
 
-  //Verify Otp
-  if (!number) {
-    return res.status(404).send({ status: "fail", msg: "Number not found" });
+  try {
+    const image = "asad";
+    const driving_license_image = "adf";
+    const { number, name, otpCode } = req.body;
+
+    //Verify Otp
+    if (!number) {
+      return res.status(404).send({ status: "fail", msg: "Number not found" });
+    }
+    const otpFound = await otpModel.findOne({ number: number });
+
+    if (!otpFound) {
+      return res.status(400).send({ status: "fail", msg: "Otp Not Sended Yet" });
+    }
+    if (otpFound.otpExpiry < Date.now) {
+      return res.status(400).send({ status: "fail", msg: "Otp expired" });
+    }
+    const isVerified = await otpFound.isValidatedOtp(otpCode);
+    if (!isVerified) {
+      return res.status(400).send({ status: "fail", msg: "Incorrect Otp" });
+    }
+    //Otp Verified
+    //Creating User
+    const userCreated = await userModel.create({
+      name: name,
+      number: number,
+      role: "deliveryBoy",
+    });
+    //User Created 
+    const deliveryBoyData = req.body;
+
+    deliveryBoyData["image"] = image;
+    deliveryBoyData["driving_license_image"] = driving_license_image;
+
+    if (
+      !image ||
+      !driving_license_image ||
+      !deliveryBoyData["name"] ||
+      !deliveryBoyData["number"] ||
+      !deliveryBoyData["aadhar"] ||
+      !deliveryBoyData["pan"] ||
+      !deliveryBoyData["upi"] ||
+      !deliveryBoyData["bike_number"]
+    ) {
+      return res.status(400).send({ status: "fail", msg: "Incomplete Data." });
+    }
+
+    const deliveryBoy = await deliveryBoyModel.create(deliveryBoyData);
+
+    return res.status(201).send({
+      status: "sucess",
+      msg: "Delivery boy created sucessfully",
+      deliveryBoy,
+    });
+  } catch (error) {
+    return res.status(400).send({
+      status: "fail",
+      msg: "Something Went WRONG",
+      error,
+    });
   }
-  const otpFound = await otpModel.findOne({ number: number });
-
-  if (!otpFound) {
-    return res.status(400).send({ status: "fail", msg: "Otp Not Sended Yet" });
-  }
-  if (otpFound.otpExpiry < Date.now) {
-    return res.status(400).send({ status: "fail", msg: "Otp expired" });
-  }
-  const isVerified = await otpFound.isValidatedOtp(otpCode);
-  if (!isVerified) {
-    return res.status(400).send({ status: "fail", msg: "Incorrect Otp" });
-  }
-  //Otp Verified
-  //Creating User
-  const userCreated = await userModel.create({
-    name: name,
-    number: number,
-    role: "deliveryBoy",
-  });
-  //User Created 
-  const deliveryBoyData = req.body;
-
-  deliveryBoyData["image"] = image;
-  deliveryBoyData["driving_license_image"] = driving_license_image;
-
-  if (
-    !image ||
-    !driving_license_image ||
-    !deliveryBoyData["name"] ||
-    !deliveryBoyData["number"] ||
-    !deliveryBoyData["aadhar"] ||
-    !deliveryBoyData["pan"] ||
-    !deliveryBoyData["upi"] ||
-    !deliveryBoyData["bike_number"]
-  ) {
-    return res.status(400).send({ status: "fail", msg: "Incomplete Data." });
-  }
-
-  const deliveryBoy = await deliveryBoyModel.create(deliveryBoyData);
-
-  return res.status(201).send({
-    status: "sucess",
-    msg: "Delivery boy created sucessfully",
-    deliveryBoy,
-  });
 };
 
 //get delivery boy
