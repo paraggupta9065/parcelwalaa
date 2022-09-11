c537a214c07e6b3647be6237bb4056e366c494c6
paraggupta9065 <paraggupta9063@gmail.com>
2022-09-11 20:53:53 +0530

    image upload

--- middleware/multerMod.js
+++ middleware/multerMod.js
@@ -1,9 +1,6 @@
 
 const multer = require("multer");
 const multerStorage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, "uploads");
-    },
     filename: (req, file, cb) => {
         const ext = file.mimetype.split("/")[1];
         cb(null, `${file.fieldname}-${Date.now()}.${"png"}`);
