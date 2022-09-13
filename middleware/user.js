56d08f397da1a42f74803e67a8b3a367c12bdc40
paraggupta9065 <paraggupta9063@gmail.com>
2022-09-13 10:03:10 +0530

    image upload

--- middleware/user.js
+++ middleware/user.js
@@ -3,21 +3,19 @@ const jwt = require("jsonwebtoken");
 
 exports.isLoggedIn = async (req, res, next) => {
   const { token } = req.headers;
-  if (!token) {
-    return next(
-      res.status(404).send({ status: "fail", msg: "Token not found." })
-    );
-  }
-
-  let decoded;
   try {
-
+    if (!token) {
+      return next(
+        res.status(404).send({ status: "fail", msg: "Token not found." })
+      );
+    }
+    let decoded;
     decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const id = decoded.id;
+    const foundUser = await user.findById({ _id: id });
+    req.user = foundUser;
   } catch (error) {
     return res.status(401).send({ status: "fail", msg: "Unauthorized" });
   }
-  const id = decoded.id;
-  const foundUser = await user.findById({ _id: id });
-  req.user = foundUser;
   next();
 };
