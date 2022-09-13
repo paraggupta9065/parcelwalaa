4e5e8582b85c3285e202ad3c1a367d5ef2ba77ec
paraggupta9065 <paraggupta9063@gmail.com>
2022-09-13 10:29:01 +0530

    image upload

--- routes/orders.js
+++ routes/orders.js
@@ -4,5 +4,6 @@ const { isShop } = require("../middleware/isShop");
 const { isLoggedIn } = require("../middleware/user");
 const router = express.Router();
 router.route("/get_orders").get(isLoggedIn, getOrders);
+router.route("/get_orders/:id").get(isLoggedIn, isShop, getOrdersShops);
 router.route("/get_orders_by_shop/:id").get(isLoggedIn, isShop, getOrdersShops);
 module.exports = router;
