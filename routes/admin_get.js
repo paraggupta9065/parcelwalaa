d70e502a12adfe82cb364b059a89a6ea3f6e0b70
paraggupta9065 <paraggupta9063@gmail.com>
2022-11-05 21:57:13 +0530

    push

--- routes/admin_get.js
+++ routes/admin_get.js
@@ -5,12 +5,12 @@ const { getCategoriesAdmin } = require("../controller/categories");
 const { getCouponsAdmin } = require("../controller/coupon");
 const { getOrdersAdmin } = require("../controller/orders");
 const { getProductsAdmin } = require("../controller/product");
-const { getShop } = require("../controller/shop");
+const { getShop, getShopsAdmin } = require("../controller/shop");
 const { isLoggedIn } = require("../middleware/user");
 const { isAdmin } = require("../middleware/isAdmin");
 const router = express.Router();
 
-router.route("/get_shops").get(isLoggedIn, isAdmin, getShop);
+router.route("/get_shops").get(isLoggedIn, isAdmin, getShopsAdmin);
 router.route("/get_categories").get(isLoggedIn, isAdmin, getCategoriesAdmin);
 router.route("/get_products").get(isLoggedIn, isAdmin, getProductsAdmin);
 router.route("/get_banners").get(isLoggedIn, isAdmin, getBannersAdmin);
